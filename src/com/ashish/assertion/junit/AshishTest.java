a46a6c309950fe524f458b5d75d133f842a63e72
ashgit1 <ashgit1@users.noreply.github.com>
2015-09-13 14:59:12 +0530

    Update AshishTest.java

--- src/com/ashish/assertion/junit/AshishTest.java
+++ src/com/ashish/assertion/junit/AshishTest.java
@@ -6,13 +6,8 @@ public class AshishTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-<<<<<<< HEAD
-System.out.println("Ashish Test is teseted");
-System.out.println("File Changed");
-=======
-System.out.println("Ashish Test is tested");
->>>>>>> d9136014506b60aecbf8d99bf0c2e95891d74c88
-
+	System.out.println("Ashish Test is teseted");
+	System.out.println("File Changed");
 	}
 
 }
